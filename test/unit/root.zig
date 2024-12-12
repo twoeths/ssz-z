866da6c2d84c335170e1ba75e24bd66e152ea8cf
twoeths <10568965+twoeths@users.noreply.github.com>
2024-12-12 17:25:25 +0700

    fix: enable tests in type/list_composite.zig

--- test/unit/root.zig
+++ test/unit/root.zig
@@ -4,10 +4,12 @@ const list_basic = @import("type/list_basic.zig");
 const vector_basic = @import("type/vector_basic.zig");
 const container = @import("type/container.zig");
 const vector_composite = @import("type/vector_composite.zig");
+const list_composite = @import("type/list_composite.zig");
 
 test {
     testing.refAllDecls(list_basic);
     testing.refAllDecls(vector_basic);
     testing.refAllDecls(container);
     testing.refAllDecls(vector_composite);
+    testing.refAllDecls(list_composite);
 }
