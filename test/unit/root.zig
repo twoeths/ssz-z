0ab1723380e9b6bf3d2cc255e18e76afaaba9a6a
twoeths <10568965+twoeths@users.noreply.github.com>
2024-12-12 17:24:26 +0700

    fix: enable tests in type/vector_composite.zig

--- test/unit/root.zig
+++ test/unit/root.zig
@@ -3,13 +3,11 @@ const testing = std.testing;
 const list_basic = @import("type/list_basic.zig");
 const vector_basic = @import("type/vector_basic.zig");
 const container = @import("type/container.zig");
-// const vector_composite = @import("type/vector_composite.zig");
-
-test "this will pass" {}
+const vector_composite = @import("type/vector_composite.zig");
 
 test {
     testing.refAllDecls(list_basic);
     testing.refAllDecls(vector_basic);
     testing.refAllDecls(container);
-    // testing.refAllDecls(vector_composite);
+    testing.refAllDecls(vector_composite);
 }
