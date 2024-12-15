4e546a99a7833ac327e700183fa896542e617200
twoeths <10568965+twoeths@users.noreply.github.com>
2024-12-15 15:26:11 +0700

    chore: add ListComposite of Container test

--- test/int/type/list_composite.zig
+++ test/int/type/list_composite.zig
@@ -7,6 +7,8 @@ const TestCase = @import("common.zig").TypeTestCase;
 const createUintType = @import("ssz").createUintType;
 const createListCompositeType = @import("ssz").createListCompositeType;
 const ByteVectorType = @import("ssz").ByteVectorType;
+const createContainerType = @import("ssz").createContainerType;
+const sha256Hash = @import("hash").sha256Hash;
 
 test "ListCompositeType - element type ByteVectorType" {
     const test_cases = [_]TestCase{
@@ -36,6 +38,55 @@ test "ListCompositeType - element type ByteVectorType" {
 
     for (test_cases[0..]) |*tc| {
         std.debug.print("ListCompositeType of Root - {s}\n", .{tc.id});
+        try TypeTest.validSszTest(&list, tc);
+    }
+}
+
+test "ListCompositeType - element type Container" {
+    const test_cases = [_]TestCase{
+        TestCase{
+            .id = "empty",
+            .serializedHex = "0x",
+            .json = "[]",
+            .rootHex = "0x96559674a79656e540871e1f39c9b91e152aa8cddb71493e754827c4cc809d57",
+        },
+        TestCase{ .id = "2 values", .serializedHex = "0x0000000000000000000000000000000040e2010000000000f1fb090000000000", .json = 
+        \\[
+        \\ {"a": "0", "b": "0"},
+        \\ {"a": "123456", "b": "654321"}
+        \\]
+        , .rootHex = "0x8ff94c10d39ffa84aa937e2a077239c2742cb425a2a161744a3e9876eb3c7210" },
+    };
+
+    const allocator = std.testing.allocator;
+    const UintType = createUintType(8);
+    const uintType = try UintType.init();
+    defer uintType.deinit();
+
+    const SszType = struct {
+        a: UintType,
+        b: UintType,
+    };
+    const ZigType = struct {
+        a: u64,
+        b: u64,
+    };
+
+    const ContainerType = createContainerType(SszType, ZigType, sha256Hash);
+    var containerType = try ContainerType.init(allocator, SszType{
+        .a = uintType,
+        .b = uintType,
+    });
+    defer containerType.deinit();
+
+    const ListCompositeType = createListCompositeType(ContainerType, ZigType);
+    var list = try ListCompositeType.init(allocator, &containerType, 128, 4);
+    defer list.deinit();
+
+    const TypeTest = @import("common.zig").typeTest(ListCompositeType);
+
+    for (test_cases[0..]) |*tc| {
+        std.debug.print("ListCompositeType of Container - {s}\n", .{tc.id});
         try TypeTest.validSszTest(&list, tc);
     }
 }
