fb26e3753e18e6c5122e3fcfe0c31504058ccb56
twoeths <10568965+twoeths@users.noreply.github.com>
2025-01-18 16:34:57 +0700

    fix: populateBitArray() to handle more than 8 bits

--- packages/persistent-merkle-tree/src/util.zig
+++ packages/persistent-merkle-tree/src/util.zig
@@ -29,6 +29,7 @@ pub fn getByteBoolArray(byte: u8) [8]bool {
 /// given a big bit_array and a gindex, populate the bit_array with the bits of gindex using getByteBoolArray
 /// return the total number of bits populated
 /// note that this is big-endian, which is the same to javascript's Number.toString(2)
+/// consumer can use stack allocation for bit_array
 pub fn populateBitArray(bit_array: []bool, gindex: u64) Error!u8 {
     const total_num_bits = numBits(gindex);
     if (bit_array.len < total_num_bits) {
@@ -36,16 +37,23 @@ pub fn populateBitArray(bit_array: []bool, gindex: u64) Error!u8 {
     }
     var buf8: [8]u8 = undefined;
     std.mem.writeInt(u64, &buf8, gindex, .little);
-    outer: for (buf8, 0..) |byte, i| {
+    // bits written starts from the right
+    var num_bits_written: usize = 0;
+    // byte_idx starts from the right most byte due to big endian
+    outer: for (buf8, 0..) |byte, byte_idx| {
         const booleans_in_byte = getByteBoolArray(byte);
         // only the last `num_bits` bits have value
-        const num_bits: usize = @intCast(total_num_bits - i * 8);
-        for (booleans_in_byte[(8 - num_bits)..], 0..) |bit, j| {
-            const bit_idx = i * 8 + j;
-            bit_array[bit_idx] = bit;
-            if (bit_idx >= total_num_bits - 1) {
-                break :outer;
-            }
+        //                         num_bits    num_bits_written
+        // |-------- ... ---------|-----------|----------------|
+        const num_bits: usize = @intCast(@min(total_num_bits - byte_idx * 8, 8));
+        // write to the last num_bits except for num_bits_written
+        for (booleans_in_byte[(8 - num_bits)..], 0..) |bit, bit_idx| {
+            const global_bit_idx = total_num_bits - num_bits_written - num_bits + bit_idx;
+            bit_array[global_bit_idx] = bit;
+        }
+        num_bits_written += num_bits;
+        if (num_bits_written >= total_num_bits) {
+            break :outer;
         }
     }
 
@@ -57,16 +65,32 @@ pub fn numBits(value: u64) u8 {
     return 64 - @clz(value);
 }
 
+// expected result is the same to NodeJS Number.toString(2)
 test "populateBitArray" {
-    var bit_array: [64]bool = [_]bool{false} ** 64;
-    // 5 = 0b101
-    var num_bits = try populateBitArray(bit_array[0..], 5);
-    try expect(num_bits == 3);
-    try std.testing.expectEqualSlices(bool, &[_]bool{ true, false, true }, bit_array[0..num_bits]);
-    // 42 = 0b101010
-    num_bits = try populateBitArray(bit_array[0..], 42);
-    try expect(num_bits == 6);
-    try std.testing.expectEqualSlices(bool, &[_]bool{ true, false, true, false, true, false }, bit_array[0..num_bits]);
+    const TestCase = struct {
+        gindex: u64,
+        num_bits: u8,
+        expected: []const bool,
+    };
+
+    const tcs = [_]TestCase{
+        // 5 = 0b101
+        .{ .gindex = 5, .num_bits = 3, .expected = &.{ true, false, true } },
+        // 42 = 0b101010
+        .{ .gindex = 42, .num_bits = 6, .expected = &.{ true, false, true, false, true, false } },
+        // 1024 = 0b10000000000
+        .{ .gindex = 1024, .num_bits = 11, .expected = &.{ true, false, false, false, false, false, false, false, false, false, false } },
+        // 1025 = 0b10000000001
+        .{ .gindex = 1025, .num_bits = 11, .expected = &.{ true, false, false, false, false, false, false, false, false, false, true } },
+        // 1_000_000 = 0b11110100001001000000
+        .{ .gindex = 1_000_000, .num_bits = 20, .expected = &.{ true, true, true, true, false, true, false, false, false, false, true, false, false, true, false, false, false, false, false, false } },
+    };
+    for (tcs[0..]) |tc| {
+        var bit_array: [64]bool = [_]bool{false} ** 64;
+        const num_bits = try populateBitArray(bit_array[0..], tc.gindex);
+        try expect(num_bits == tc.num_bits);
+        try std.testing.expectEqualSlices(bool, tc.expected, bit_array[0..num_bits]);
+    }
 }
 
 test "numBits" {
